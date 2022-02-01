812b08c53ddc2563ae98ca502112932ce3858d41
zainahda <zainahda@gmail.com>
2022-02-01 16:52:45 +0700

    Niomic Tugas 4 Js

---
+++ tugas4.js
@@ -0,0 +1,19 @@
+let adam = 175; 
+    bayu = 165;
+    andi = 155;
+    
+
+function tinggiSiswa(x) {
+  if(x == 175) {
+    console.log("Siswa tertinggi ke-1 di kelas");
+  }else if (x == 165) {
+    console.log("Siswa tertinggi ke-2 di kelas");
+  } else {
+    console.log("Siswa terpendek di kelas");
+  }
+}
+
+tinggiSiswa(adam);
+tinggiSiswa(bayu);
+tinggiSiswa(andi);
+
